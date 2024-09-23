4191939d9721db49b74857d2f5908268dd4e67ce
fruizguinazu <fruizguinazu@udesa.edu.ar>
2024-09-23 20:51:13 -0300

    Search y Grid peliculas 2

---
+++ src/pages/Search.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Component } from 'react';
+import PeliGrid2 from '../components/PeliGrid2/PeliGrid2';
+import Loader from '../components/Loader/Loader';
+
+
+class ResultadoBusqueda extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     peliculas: [],
+     isLoading:true,
+   };
+ }
+
+
+ componentDidMount() {
+   this.setState({
+     isLoading:true,
+   })
+   fetch(`https://api.themoviedb.org/3/search/movie?query=${this.props.location.state.query}&api_key=f2d31985b9fc9e720758bcc82e3c955b`)
+     .then((res) => res.json())
+     .then((data) => {
+       this.setState({
+       peliculas: data.results,
+       isLoading:false,
+     })
+   })
+     .catch((e) => console.log(e))
+ }
+
+
+ render() {
+   return (
+     <>
+       {!this.state.isLoading ? (
+         <div>
+           <h2>Resultados de tu busqueda:</h2>
+           <PeliGrid2 datos={this.state.peliculas}/>
+         </div>
+       ):(
+         <Loader/>
+       )
+       }
+     </>
+   )
+ }
+
+
+}
+export default ResultadoBusqueda;
