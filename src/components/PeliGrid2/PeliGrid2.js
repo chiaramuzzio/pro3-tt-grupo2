3391cc98d08a3dac65b72f822da496d1b02c94bf
melinebirnisan <mbirnisan@udesa.edu.ar>
2024-09-23 18:17:10 -0300

    cambios minimos

---
+++ src/components/PeliGrid2/PeliGrid2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Peli from '../Peli/Peli';
+
+const PeliGrid2 = ({datos}) => {
+    return (
+        <>
+        <section>
+            {datos.map((pelicula, idx)=> <Peli pelicula = {pelicula} key = {idx}/>)}
+        </section>
+        </>
+    );
+}
+
+export default PeliGrid2
+
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,38 +3,46 @@ import PeliGrid from "../components/PeliGrid/PeliGrid";
 import Loader from "../components/Loader/Loader";
 import SearchForm from "../components/SearchForm/SearchForm";
 
+
 const api_key = "378786c706182646715863ed0e6d66cc";
 const urlPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
 const urlEstrenos = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`;
 
+
 class Home extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        loading: true 
-    };
+
+ constructor(props) {
+   super(props);
+   this.state = {
+       loading: true
+   };
 }
+
 
 componentDidMount() {
-  this.setState({ loading: false }); 
+ this.setState({ loading: false });
 }
 
 
-  render() {
 
-    return (
-      <>
-      {this.state.loading ? (<Loader />) : 
-      (<>
-      <SearchForm history={this.props.history} />
-      <PeliGrid url={urlEstrenos} titulo="Estrenos" />
-      <PeliGrid url={urlPopulares} titulo="Peliculas Populares" />
-      </>)}
-        
-      </>
-    );
-  }
+
+ render() {
+
+
+   return (
+     <>
+     {this.state.loading ? (<Loader />) :
+     (<>
+     <SearchForm history={this.props.history} />
+     <PeliGrid url={urlEstrenos} titulo="Estrenos" todas ="/estrenos"/>
+     <PeliGrid url={urlPopulares} titulo="Peliculas Populares" todas ="/populares" />
+     </>)}
+      
+     </>
+   );
+ }
 }
+
 
 export default Home;
