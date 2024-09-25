ead6a6c6588f6066171182d701b9a927d0c37008
melinebirnisan <mbirnisan@udesa.edu.ar>
2024-09-25 19:03:23 -0300

    vermasopopulares

---
+++ src/pages/VerMasPopulares.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import PeliGrid from '../components/PeliGrid/PeliGrid';
+;
+
+
+class VerMasPopulares extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            peliculas: [],
+            peliculasFiltradas: [],
+            valorFiltrado: "",
+            paginaActual: 1,
+        };
+    }
+
+
+    componentDidMount() {
+        this.fetchMovies();
+    }
+
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.category !== prevProps.match.params.category) {
+            this.fetchMovies();
+        }
+    }
+
+
+    fetchMovies() {
+        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=31e421d77201e7a1eefe33f85b67fa3b&page=${this.state.paginaActual}`)
+            .then(response => response.json())
+            .then(data => this.setState({
+                peliculas: data.results,
+                peliculasFiltradas: data.results,
+                paginaActual: this.state.paginaActual + 1,
+            }))
+            .catch(err => console.log(err));
+    }
+
+
+    handleFilter(e) {
+        const userValue = e.target.value;
+        this.setState({
+            valorFiltrado: userValue,
+            peliculasFiltradas: this.state.peliculas.filter((pelicula) =>
+                pelicula.title.toLowerCase().includes(userValue.toLowerCase())
+            ),
+        });
+    }
+
+
+    handleResetFilter() {
+        this.setState({
+            valorFiltrado: "",
+            peliculasFiltradas: this.state.peliculas,
+        });
+    }
+
+
+    handleLoadMore() {
+        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=31e421d77201e7a1eefe33f85b67fa3b&page=${this.state.paginaActual}`)
+            .then(response => response.json())
+            .then(data => this.setState({
+                peliculas: this.state.peliculas.concat(data.results),
+                peliculasFiltradas: this.state.peliculasFiltradas.concat(data.results),
+                paginaActual: this.state.paginaActual + 1,
+            }))
+            .catch(err => console.log(err));
+    }
+
+
+    render() {
+        const { titulo } = this.props;
+        return (
+            <>
+                <input
+                    type="text"
+                    value={this.state.valorFiltrado}
+                    onChange={(e) => this.handleFilter(e)}
+                    placeholder="Filtrar películas"
+                />
+                <button onClick={() => this.handleResetFilter()}>Eliminar Filtro</button>
+                <PeliGrid titulo={titulo} peliculas={this.state.peliculasFiltradas} boton={false} />
+                <button onClick={() => this.handleLoadMore()}>Cargar Más</button>
+            </>
+        );
+    }
+}
+
+
+export default VerMasPopulares;
