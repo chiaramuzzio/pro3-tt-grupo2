1f163dfeb5c23c7aa30cd0d0fcf9d527ed9e442f
fruizguinazu <fruizguinazu@udesa.edu.ar>
2024-09-27 10:01:53 -0300

    css NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 import "../components/NotFound/notFound.css";
 import Loader from "../components/Loader/Loader";
 
+
 class NotFound extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: true 
+            loading: true
         };
     }
+
 
     componentDidMount() {
         this.setState({ loading: false }); }
 
+
     render() {
         return (
             <>
-            {this.state.loading ? (<Loader />) : 
+            {this.state.loading ? (<Loader />) :
             (
                 <div>
-                    <h1 className="NotFound">404: Not Found</h1>
-                    <h2 className="NotFound">Parece que no existe esta pagina!</h2>
+                    {/* <h1 className="NotFound">404: Not Found</h1>
+                    <h2 className="NotFound">Parece que no existe esta pagina!</h2> */}
                     <img src="https://cdn.dribbble.com/users/718859/screenshots/3267029/jisunpark_404-error.gif" className="gifNotFound" alt="404 Not Found"/>
                 </div>
             )}
@@ -28,5 +31,6 @@ class NotFound extends Component {
         );
     }
 }
+
 
 export default NotFound;
